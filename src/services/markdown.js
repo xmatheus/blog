d5f8c23b009985e6e5a6943dcc3b8109924ea2b5
xmatheus <matheuscorreia559@gmail.com>
2021-08-04 17:21:23 -0400

    refactor: improving code legibility

--- src/services/markdown.js
+++ src/services/markdown.js
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import remark from 'remark'
 import html from 'remark-html'
 import footnotes from 'remark-footnotes'
 import remarkGfm from 'remark-gfm'
 import prism from 'remark-prism'
 
-export async function toHTML(markdown) {
+export default async function toHTML(markdown) {
   const result = await remark()
     .use(footnotes)
     .use(remarkGfm)
@@ -17,5 +18,3 @@ export async function toHTML(markdown) {
     .toString()
     .replace(/<img (?<rest>.*)">{1}/gm, `<img $1" loading="lazy">`)
 }
-
-export default { toHTML }
