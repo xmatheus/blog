e0e7efed724b42a6eaca0bfaa584f1d1d83316cb
xmatheus <matheuscorreia559@gmail.com>
2022-04-03 18:38:47 -0300

    chore: update next headers

--- next.config.js
+++ next.config.js
@@ -25,5 +25,23 @@ module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     dest: 'public'
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, must-revalidate'
+          }
+        ]
+      }
+    ]
   }
 })
